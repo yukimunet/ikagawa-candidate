b38c9365e974fad903e96c09d7b37563d793aed8
Tsutomu Kawamura <cognitom@gmail.com>
2015-02-25 10:01:55 +0900

    candidate.jsonを読むように変更

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-riot.mount('*');
+function ajax(url, fn) {
+  var req = new XMLHttpRequest()
+
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) fn(req.responseText)
+  }
+  req.open('GET', url, true)
+  req.send('')
+}
+
+ajax('./candidate.json', function(data) {
+  riot.mount('app', JSON.parse(data))
+})
